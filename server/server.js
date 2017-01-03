ca5405de4de97dd2eb54221eee7d155a8ebd9153
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-01-03 09:44:17 +0100

    add get/todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,15 @@ res.send(doc);
 });
 });
 
+app.get('/todos', (req, res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  }, (e)=>{
+    res.status(400).send(e);
+  });
+});
+
+
 app.listen(3000,()=>{
   console.log('Start on port 3000');
 });
