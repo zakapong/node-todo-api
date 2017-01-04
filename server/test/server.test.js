97d992281aca1d3fd9fc9d29ea10343164210706
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-01-04 10:00:57 +0100

    add test ca´ses for GET /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,14 +1,17 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID}= require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 
 const todos=[{
+  _id: new ObjectID(),
   text: 'First test todo'
-},{
-  text:'Second test todo'
+}, {
+    _id: new ObjectID(),
+    text:'Second test todo'
 }];
 
 // beforeEach((done) => {
@@ -76,4 +79,35 @@ describe('GET/todos', ()=>{
     })
   .end(done);
   });
+});
+
+
+describe('GET/todos/:id', ()=>{
+it('should return todo doc', (done)=>{
+request(app)
+.get(`/todos/${todos[0]._id.toHexString()}`)
+.expect(200)
+.expect((res)=>{
+  expect(res.body.todo.text).toBe(todos[0].text);
+})
+.end(done);
+
+  });
+it('should return 404 if todo not found', (done)=>{
+  var hexId= new ObjectID().toHexString();
+request(app)
+.get(`/todos/${hexId}`)
+.expect(404)
+
+.end(done)
+});
+
+it('should return 404 if todo not found', (done)=>{
+  request(app)
+  .get('/todos/123abc')
+  .expect(404)
+  .end(done);
+});
+
+
 });
