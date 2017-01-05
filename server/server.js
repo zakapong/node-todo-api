2c9e031dab2ac1db3343f03408edc5aac3937a5c
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-01-05 09:11:24 +0100

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser= require('body-parser');
+const _ =require('lodash');
+const express = require('express');
+const bodyParser= require('body-parser');
 const {ObjectID}= require('mongodb');
 
 
@@ -68,6 +69,32 @@ app.delete('/todos/:id',(req, res)=>{
     });
 
 });
+
+app.patch('/todos/:id',(req, res)=>{
+var id = req.params.id;
+var body= _.pick(req.body,['text','completed']);
+if(!ObjectID.isValid(id)){
+  res.status(404).send();
+}
+
+if(_.isBoolean(body.completed)&& body.completed){
+  body.completedAt= new Date().getTime();
+}else{
+  body.completed= false;
+  body.completedAt= null;
+
+}
+Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+if(!todo){
+  return res.status(404).send();
+}
+  res.send({todo});
+}).catch((e)=>{
+res.status(400).send();
+})
+
+});
+
 
 app.listen(port,()=>{
   console.log(`Start on port ${port}`);
