3363830c4cd2002b333eb0f2a44b561fb466013f
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-01-07 21:31:02 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,31 @@
 const {SHA256}= require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt= require('bcryptjs');
 
-var data={
-  id: 10
-};
-var token=jwt.sign(data, '123abc');
-console.log(token);
+var password='123abc!';
+//bcrypt.genSalt(10, (err, salt)=>{
+  //bcrypt.hash(password, salt, (err, hash)=>{
+    //console.log(hash);
+  //});
+//});
 
-var decoded= jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+
+var hashedPassword='$2a$10$Zt3P3LfgIRxB/gEwlpkXrO8PukFopc4o4ksfsjZrITG6jAf6HU.Sy';
+
+bcrypt.compare(password, hashedPassword,(err, res)=>{
+  console.log(res);
+});
+
+
+
+//var data={
+//  id: 10
+//};
+//var token=jwt.sign(data, '123abc');
+//console.log(token);
+
+//var decoded= jwt.verify(token, '123abc');
+//console.log('decoded', decoded);
 //var message= 'I am user number 3';
 
 //var hash= SHA256(message).toString();
--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 const validator=require('validator');
 const jwt= require('jsonwebtoken');
 const _ = require('lodash');
-
+const bcrypt= require('bcryptjs');
 var UserSchema= new mongoose.Schema({
   email:{
     type: String,
@@ -66,6 +66,21 @@ return Promise.reject();
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.pre('save', function(next){
+  var user= this;
+  if(user.isModified('password')){
+  bcrypt.genSalt(10, (err, salt)=>{
+    bcrypt.hash(user.password, salt, (err, hash)=>{
+      user.password=hash;
+      next();
+    });
+  });
+  }else{
+    next();
+  }
+
+});
 
 var User=mongoose.model('User', UserSchema);
 module.exports={User};
